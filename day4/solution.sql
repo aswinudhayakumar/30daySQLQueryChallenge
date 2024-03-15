2d54f13e5ff3164c3af3a7509b19289287b843ae
Aswin Udhayakumar <aswiniu03@gmail.com>
2024-03-15 19:00:14 +0530

    Add day4 question and solution

---
+++ day4/solution.sql
@@ -0,0 +1,4 @@
+-- solution for question type 1
+select min(id) id, min(name) name, min(location) location from q4_data;
+-- solution for question type 2
+select max(id) id, min(name) name, min(location) location from q4_data;
